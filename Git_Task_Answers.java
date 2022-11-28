d6e5194362fd7d428698d1c4ccdc8f2e812e5629
220107130 <118952899+220107130@users.noreply.github.com>
2022-11-28 17:33:36 +0600

    Add files via upload

---
+++ Git_Task_Answers.java
@@ -0,0 +1,11 @@
+public class Git_Task_Answers {
+    public static void main(String[] args) {
+        System.out.println("Name: Beibarys");
+        System.out.println("Surname: Serikkaly");
+        System.out.println("Id: 220107130");
+        System.out.println("Hobby: Football");
+        System.out.println("How much do you like ICT(from 1-10)): 10");
+        System.out.println("Rate this task from 1 to 10: 10");
+        System.out.println("Am I the best teacher?: Of course");
+    }
+}
